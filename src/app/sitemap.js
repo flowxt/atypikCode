8ff7be2b6a17e810cb772d15386886952b998e08
florian <florianbarjon@icloud.com>
2025-05-24 11:59:35 +0200

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -15,6 +15,12 @@ export default function sitemap() {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/informatique`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: "monthly",
